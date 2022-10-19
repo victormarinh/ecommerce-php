eb32c48b50144595801cec33e7c000fdb4b929ac
Victor Marinho <augustovictormarinho@gmail.com>
2022-10-19 00:09:35 -0300

    update general

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 
 require_once("vendor/autoload.php");
 
+use Hcode\DB;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	echo "Mama mia";
+
+	$sql = new Sql();
+
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
