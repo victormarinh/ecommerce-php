4a1e308b588c750ce68468560a5151b9ccce2e51
Victor Marinho <augustovictormarinho@gmail.com>
2022-10-19 23:20:11 -0300

    Aplicando template de AdminLTE

--- index.php
+++ index.php
@@ -3,18 +3,25 @@
 require_once("vendor/autoload.php");
 
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	//echo "Ok";
-
+	
 	$page = new Page();
 
 	$page->setTpl("index", array(), false);
+	
+});
+
+$app->get('/admin', function() {
+
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin->setTpl("index", array(), false);
 	
 });
 
