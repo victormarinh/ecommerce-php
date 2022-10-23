d84fb08462792e9ffed5143b9f3fa12613878db1
Victor Marinho <augustovictormarinho@gmail.com>
2022-10-23 19:16:07 -0300

    commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use Hcode\Page;
 use Hcode\PageAdmin;
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -22,6 +23,27 @@ $app->get('/admin', function() {
 	$pageAdmin = new PageAdmin();
 
 	$pageAdmin->setTpl("index", array(), false);
+	
+});
+
+$app->get('/admin/login', function() {
+
+	$pageAdmin = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$pageAdmin->setTpl("login", array(), false);
+	
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST['login'], $_POST['password']);
+
+	header("Location: /admin");
+
+	exit;
 	
 });
 
